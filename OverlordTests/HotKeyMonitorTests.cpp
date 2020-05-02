d7d6dd8778719487dd92a6bbae219c7988eb5c63
Valrandir <valrandir@voxcel.net>
2020-05-02 18:07:01 -0400

    Fix clang-format for Native Unit Test Project

--- OverlordTests/HotKeyMonitorTests.cpp
+++ OverlordTests/HotKeyMonitorTests.cpp
@@ -9,37 +9,35 @@ void HotKeyCallback(int id, void* userdata)
 	++*cb_count;
 }
 
-TEST_CLASS(HotKeyMonitorTests){
-	public:
-		TEST_METHOD(Dispatch_TwoHotKeyPost_CallbackTwice)
-		{
-			//Arrange
-			HotKeyMonitor hkm;
-			int cb_count{};
-			auto id = hkm.AddHotKey(HotKeyCallback, &cb_count, VK_NUMPAD5);
-			PostMessage(NULL, WM_HOTKEY, id, 0);
-			PostMessage(NULL, WM_HOTKEY, id, 0);
+TEST_CLASS(HotKeyMonitorTests) {
+public:
+	TEST_METHOD(Dispatch_TwoHotKeyPost_CallbackTwice) {
+		//Arrange
+		HotKeyMonitor hkm;
+		int cb_count{};
+		auto id = hkm.AddHotKey(HotKeyCallback, &cb_count, VK_NUMPAD5);
+		PostMessage(NULL, WM_HOTKEY, id, 0);
+		PostMessage(NULL, WM_HOTKEY, id, 0);
 
-			//Act
-			hkm.Dispatch();
+		//Act
+		hkm.Dispatch();
 
-			//Assert
-			Assert::AreEqual(2, cb_count);
-		}
+		//Assert
+		Assert::AreEqual(2, cb_count);
+	}
 
-		TEST_METHOD(Dispatch_HotKeyRemoved_DoNotCallback)
-		{
-			//Arrange
-			HotKeyMonitor hkm;
-			int cb_count{};
-			auto id = hkm.AddHotKey(HotKeyCallback, &cb_count, VK_NUMPAD5);
-			hkm.RemoveHotKey(id);
-			PostMessage(NULL, WM_HOTKEY, id, 0);
+	TEST_METHOD(Dispatch_HotKeyRemoved_DoNotCallback) {
+		//Arrange
+		HotKeyMonitor hkm;
+		int cb_count{};
+		auto id = hkm.AddHotKey(HotKeyCallback, &cb_count, VK_NUMPAD5);
+		hkm.RemoveHotKey(id);
+		PostMessage(NULL, WM_HOTKEY, id, 0);
 
-			//Act
-			hkm.Dispatch();
+		//Act
+		hkm.Dispatch();
 
-			//Assert
-			Assert::AreEqual(0, cb_count);
-		}
+		//Assert
+		Assert::AreEqual(0, cb_count);
+	}
 };
